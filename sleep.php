cb092865f8ec9d37fc2d7c2f5e648e1dcc142565
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2016-05-04 10:53:48 -0500

    remove redundancies

--- sleep.php
+++ sleep.php
@@ -24,8 +24,6 @@ else
 <body>
 <?php
 
-  date_default_timezone_set("CST6CDT");
-  $currentDay = getdate()["weekday"];
   echo "<h1>Today is...</h1>";
   sleep(1);
   echo "<h2>$currentDay.</h2> \n";
